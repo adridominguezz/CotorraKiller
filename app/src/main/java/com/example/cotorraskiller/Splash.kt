c2cd36858278d0b304f41122e8e41fdf2ac46682
adridominguezz <dominguezmorenoadrian@gmail.com>
2023-12-19 23:33:26 +0100

    19/12 - 23:30

---
+++ app/src/main/java/com/example/cotorraskiller/Splash.kt
@@ -0,0 +1,13 @@
+package com.example.cotorraskiller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        
+    }
+}
